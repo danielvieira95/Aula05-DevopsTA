d14d93740cadea7f6b0cae134ff50a2e6485569b
danielvieira95 <engdanielvieira95@gmail.com>
2026-09-03 15:22:46 -0300

    Aula 05 Devops TA

---
+++ junittest/src/main/java/com/senai/br/App.java
@@ -0,0 +1,13 @@
+package com.senai.br;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
